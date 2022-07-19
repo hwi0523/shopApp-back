931afd44f04a83f50c7013568b64a69887ddc83c
hwi0523 <101933886+hwi0523@users.noreply.github.com>
2022-07-19 18:06:04 +0900

    0719 8교시

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -9,6 +9,22 @@ class ApiController extends Controller {
     public function productInsert() {
         $json = getJson();
         print_r($json);
-        return $this->model->productInsert($json);
+        return [_RESULT => $this->model->productInsert($json)];
+    }
+
+    public function productList2(){
+        $result = $this->model->productList2();
+        return $result === false ? [] : $result;
+    }
+
+    public function productDetail() {
+        $urlPaths = getUrlPaths();
+        if(!isset($urlPaths[2])) {
+            exit();
+        }
+        $param = [
+            'product_id' => intval($urlPaths[2])
+        ];
+        return $this->model->productDetail($param);
     }
 }
